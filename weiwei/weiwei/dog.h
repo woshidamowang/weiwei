599ad15454f8d1fd2ef8f942ac76e0921791f059
woshidamowang <251262886@qq.com>
2017-09-03 11:57:51 +0800

    添加dog文件

---
+++ weiwei/weiwei/dog.h
@@ -0,0 +1,13 @@
+//
+//  dog.h
+//  weiwei
+//
+//  Created by 陶杨 on 17/9/3.
+//  Copyright © 2017年 taoyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@end
---
+++ weiwei/weiwei/dog.m
@@ -0,0 +1,13 @@
+//
+//  dog.m
+//  weiwei
+//
+//  Created by 陶杨 on 17/9/3.
+//  Copyright © 2017年 taoyang. All rights reserved.
+//
+
+#import "dog.h"
+
+@implementation dog
+
+@end
